589ade1f276e25d618d478f807c754503d517e7a
Hamzah Aldaamas <86842077+hamzahhisham306@users.noreply.github.com>
2022-09-11 17:28:58 +0300

    Update index.js

--- modules/index.js
+++ modules/index.js
@@ -3,10 +3,10 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const food = require('./food.model')
 
-// const POSTGRES_URL = process.env.DATABASE_URL || "postgresql://waleed:123123@localhost:5432/rest";
+// const POSTGRES_URL = process.env.HEROKU_POSTGRESQL_MAUVE_URL || "postgresql://waleed:123123@localhost:5432/rest";
 
 // postgres://localhost:5432/postgres (Mac user)
-const POSTGRES_URL = process.env.DATABASE_URL || "postgresql://hamzah:123456789@localhost:5432/hamzah";
+const POSTGRES_URL = process.env.HEROKU_POSTGRESQL_MAUVE_URL || "postgresql://hamzah:123456789@localhost:5432/hamzah";
 
 const sequelizeOption = {
   dialectOptions: {
